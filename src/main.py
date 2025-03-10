eb874ab21588df84010efa9c22e9e582b414d7cc
Joy <129235760+JoysGitHubPortfolio@users.noreply.github.com>
2025-03-10 19:43:30 +0000

    Output model evaluation metrics

--- src/main.py
+++ src/main.py
@@ -2,22 +2,14 @@ from openai import OpenAI
 from agent import MakeMyAgent
 from smart_functions import SmartFunction
 from prompt import sentiment_agent_role, outcome_agent_role, assessor_agent_role
-import os, json
+import os, json, pandas as pd, numpy as np
 
-with open('../transcripts/transcript_0.txt') as f:
-    text = f.readlines()
-
-# Filter lines containing "Member"
-member_body = [line.strip().removeprefix('Member: ') 
-               for line in text if 'Member: ' in line]
-member_body = ' '.join(member_body)
-print(member_body)
 
 
 # Initialise client by passing key known to you & Initialise SmartFunction
-API_KEY = os.getenv(input('Enter your environment variable: '))
-client = OpenAI(api_key=API_KEY) 
-sf = SmartFunction()
+# API_KEY = os.getenv(input('Enter your environment variable: '))
+# client = OpenAI(api_key=API_KEY) 
+# sf = SmartFunction()
 
 with open("../config/response_format_assessor.json", "r") as f:
     assessor_response_format = json.load(f)
@@ -26,73 +18,70 @@ with open("../config/response_format_sentiment.json", "r") as f:
 with open("../config/response_format_outcome.json", "r") as f:
     outcome_response_format = json.load(f)
 
-# Define model parameters
-model_params = {
-    "temperature": 0.5,
-    "max_tokens": 2048,
-    "top_p": 0.8,
-    "frequency_penalty": 1,
-    "presence_penalty": 1
-}
+# Choose the parameters where the SSE is lowest & set globally
+model_params_data = pd.read_csv('../output/grid_search_results.csv')
+best_model_params = model_params_data.loc[model_params_data['sse'].idxmin()]
+model_params = best_model_params
+print(model_params)
 
-# Get my sentiment analysis
-sentiment_agent = MakeMyAgent(
-    model = "gpt-4o-mini",
-    client = client, 
-    model_params = model_params,
-    role = sentiment_agent_role,
-    response_format = sentiment_response_format,
-    user_input = member_body
-)
-sentiment_output = sentiment_agent.run()
-json_sentiment_output = json.loads(sentiment_output)
-print(json_sentiment_output)
+# # Get my sentiment analysis
+# sentiment_agent = MakeMyAgent(
+#     model = "gpt-4o-mini",
+#     client = client, 
+#     model_params = model_params,
+#     role = sentiment_agent_role,
+#     response_format = sentiment_response_format,
+#     user_input = member_body
+# )
+# sentiment_output = sentiment_agent.run()
+# json_sentiment_output = json.loads(sentiment_output)
+# print(json_sentiment_output)
 
-# Engineers can debug with quality analysis
-assessor_agent = MakeMyAgent(
-    model = "gpt-4o-mini",
-    client = client, 
-    model_params = model_params,
-    role = assessor_agent_role,
-    response_format = assessor_response_format,
-    user_input = sentiment_output
-)
-assessor_output = assessor_agent.run()
-json_assessor_output = json.loads(assessor_output)
-print(json_assessor_output)
+# # Engineers can debug with quality analysis
+# assessor_agent = MakeMyAgent(
+#     model = "gpt-4o-mini",
+#     client = client, 
+#     model_params = model_params,
+#     role = assessor_agent_role,
+#     response_format = assessor_response_format,
+#     user_input = sentiment_output
+# )
+# assessor_output = assessor_agent.run()
+# json_assessor_output = json.loads(assessor_output)
+# print(json_assessor_output)
 
-# check instance of improvement
-sentiment_improvement = sf.is_improvement_required(json_sentiment_output, json_assessor_output)
-print(sentiment_improvement)
-print()
+# # check instance of improvement
+# sentiment_improvement = sf.is_improvement_required(json_sentiment_output, json_assessor_output)
+# print(sentiment_improvement)
+# print()
 
 
-# Create another agent to check if customer outcome as reached
-outcome_agent = MakeMyAgent(
-    model = "gpt-4o-mini",
-    client = client, 
-    model_params = model_params,
-    role = outcome_agent_role,
-    response_format = outcome_response_format,
-    user_input = member_body
-)
-outcome_output = outcome_agent.run()
-json_outcome_output = json.loads(outcome_output)
-print(json_outcome_output)
+# # Create another agent to check if customer outcome as reached
+# outcome_agent = MakeMyAgent(
+#     model = "gpt-4o-mini",
+#     client = client, 
+#     model_params = model_params,
+#     role = outcome_agent_role,
+#     response_format = outcome_response_format,
+#     user_input = member_body
+# )
+# outcome_output = outcome_agent.run()
+# json_outcome_output = json.loads(outcome_output)
+# print(json_outcome_output)
 
-outcome_assessor_agent = MakeMyAgent(
-    model = "gpt-4o-mini",
-    client = client,
-    model_params = model_params,
-    role = assessor_agent_role,
-    response_format = assessor_response_format,
-    user_input = outcome_output
-)
-outcome_assesor_output = outcome_assessor_agent.run()
-json_outcome_assessor_output = json.loads(outcome_assesor_output)
-print(json_outcome_assessor_output)
+# outcome_assessor_agent = MakeMyAgent(
+#     model = "gpt-4o-mini",
+#     client = client,
+#     model_params = model_params,
+#     role = assessor_agent_role,
+#     response_format = assessor_response_format,
+#     user_input = outcome_output
+# )
+# outcome_assesor_output = outcome_assessor_agent.run()
+# json_outcome_assessor_output = json.loads(outcome_assesor_output)
+# print(json_outcome_assessor_output)
 
-# check instance of improvement
-outcome_improvement = sf.is_improvement_required(json_outcome_output, json_outcome_assessor_output)
-print(outcome_improvement)
-print()
+# # check instance of improvement
+# outcome_improvement = sf.is_improvement_required(json_outcome_output, json_outcome_assessor_output)
+# print(outcome_improvement)
+# print()
--- src/pseudo_optimisation.py
+++ src/pseudo_optimisation.py
@@ -1,83 +1,3 @@
-# from openai import OpenAI
-# from agent import MakeMyAgent
-# from smart_functions import SmartFunction
-# from prompt import sentiment_agent_role, outcome_agent_role, assessor_agent_role
-# import pandas as pd
-# import numpy as np
-# import os, json 
-
-# # Initialise client by passing key known to you & Initialise SmartFunction & parameters
-# API_KEY = os.getenv(input('Enter your environment variable: '))
-# client = OpenAI(api_key=API_KEY) 
-# sf = SmartFunction()
-
-# with open("../config/response_format_assessor.json", "r") as f:
-#     assessor_response_format = json.load(f)
-# with open("../config/response_format_sentiment.json", "r") as f:
-#     sentiment_response_format = json.load(f)
-# with open("../config/response_format_outcome.json", "r") as f:
-#     outcome_response_format = json.load(f)
-
-# # Define model parameters
-# model_params = {
-#     "temperature": 0.5,
-#     "max_tokens": 2048,
-#     "top_p": 0.8,
-#     "frequency_penalty": 1,
-#     "presence_penalty": 1
-# }
-
-# # Load data
-# df = pd.read_csv('../output/transcripts_labelled.csv')
-# df = df.dropna(subset=['ground_positive', 'ground_neutral', 'ground_negative'])
-# print(df.head())
-
-# model_sentiments = {}
-# limit = len(df)
-# for transcript_id, member_body in zip(df['transcript_id'][0:limit], 
-#                                       df['member_body'][0:limit]):
-#     print(member_body)
-
-#     sentiment_agent = MakeMyAgent(
-#         model = "gpt-4o-mini",
-#         client = client, 
-#         model_params = model_params,
-#         role = sentiment_agent_role,
-#         response_format = sentiment_response_format,
-#         user_input = member_body
-#     )
-#     predicted_sentiment = sentiment_agent.run()
-#     json_predicted_sentiment = json.loads(predicted_sentiment)
-
-#     predictions = {}
-#     predictions['positive'] = json_predicted_sentiment['positive']
-#     predictions['neutral'] = json_predicted_sentiment['neutral']
-#     predictions['negative'] = json_predicted_sentiment['negative']
-#     model_sentiments[transcript_id] = predictions
-
-
-# # Convert dictionary to DataFrame
-# predictions_df = pd.DataFrame.from_dict(model_sentiments, orient='index')
-# predictions_df.reset_index(inplace=True)
-# predictions_df.rename(columns={'index': 'transcript_id', 
-#                                'positive': 'predicted_positive', 
-#                                'neutral': 'predicted_neutral', 
-#                                'negative': 'predicted_negative'}, inplace=True)
-
-# # Get SSE as metric for model
-# df = df.merge(predictions_df, on='transcript_id', how='left')
-# df['sse'] = df.apply(lambda row: (row['ground_positive'] - row['predicted_positive'])**2 + 
-#                                 (row['ground_neutral'] - row['predicted_neutral'])**2 + 
-#                                 (row['ground_negative'] - row['predicted_negative'])**2, axis=1)
-# df['psuedo_accuracy'] = (1 - df['sse'])/3
-# df.to_csv('../output/transcripts_psuedo_optimisation.csv')
-
-# model_sse = np.sum(df['sse'])
-# model_pseudo_accuracy = np.mean(df['psuedo_accuracy'])
-
-# print('SSE', model_sse)
-# print('Pseudo Accuracy', model_pseudo_accuracy)
-
 import pandas as pd
 import numpy as np
 import itertools
@@ -85,7 +5,6 @@ import json
 import os
 from openai import OpenAI
 from agent import MakeMyAgent
-from smart_functions import SmartFunction
 from prompt import sentiment_agent_role
 
 class SentimentOptimizer:
